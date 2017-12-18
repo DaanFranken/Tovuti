646a31f1e86f746cf3c783b9713ec495a137e420
DaanFranken <dgmfranken@gmail.com>
2017-12-18 13:47:23 +0100

    commit

---
+++ login.php
@@ -0,0 +1,18 @@
+<head>
+<link rel="stylesheet" type="text/css" href="style/style.css">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+</head>
+<div class="w3-container w3-teal">
+  <h2>Login</h2>
+</div>
+
+<form class="w3-container">
+  <label class="w3-text-teal"><b>First Name</b></label>
+  <input class="w3-input w3-border w3-light-grey" type="text">
+
+  <label class="w3-text-teal"><b>Last Name</b></label>
+  <input class="w3-input w3-border w3-light-grey" type="text">
+  <br/>
+  <button class="w3-btn w3-blue-grey">Register</button>
+</form>
