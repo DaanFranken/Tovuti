8d6db926fcd3fafcdad212aadc594ebba6dfe2ba
DaanFranken <dgmfranken@gmail.com>
2017-12-18 14:29:53 +0100

    login form

--- login.php
+++ login.php
@@ -4,15 +4,35 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
 </head>
 <div class="w3-container w3-teal">
+
   <h2>Login</h2>
 </div>
+<form class="w3-container" method="POST">
+  <label class="w3-text-teal"><b>Username</b></label>
+  <input class="w3-input w3-border w3-light-grey" name="username" type="text">
 
-<form class="w3-container">
-  <label class="w3-text-teal"><b>First Name</b></label>
-  <input class="w3-input w3-border w3-light-grey" type="text">
-
-  <label class="w3-text-teal"><b>Last Name</b></label>
-  <input class="w3-input w3-border w3-light-grey" type="text">
+  <label class="w3-text-teal"><b>Password</b></label>
+  <input class="w3-input w3-border w3-light-grey" name="password" type="Password">
   <br/>
-  <button class="w3-btn w3-blue-grey">Register</button>
+  <input type="submit" class="w3-btn w3-blue-grey" value="Login" name="loginSubmit">
 </form>
+
+<?php
+if($misc->readVar('POST','username') && $misc->readVar('POST','password'))
+{
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	$user = new User();
+	
+	if($user->login($username,$password))
+	{
+		echo "login success";
+	}
+	else
+	{
+		echo "login failed";
+	}
+} 
+
+?>
+
