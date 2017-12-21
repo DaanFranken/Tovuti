ef45432a5e6be6b85e01aea2e46066e4703f9890
stijnklomp <32292824+stijnklomp@users.noreply.github.com>
2017-12-21 12:22:55 +0100

    Balsamiq Mockups update
    https://balsamiq.com/download/

--- login.php
+++ login.php
@@ -4,24 +4,31 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
 </head>
 <div class="w3-container w3-teal">
-
-  <h2>Inloggen</h2>
+<h2>Inloggen</h2>
 </div>
-<form class="w3-container" method="POST">
-  <label class="w3-text-teal"><b>Gebruikersnaam of student nummer</b></label>
-  <input class="w3-input w3-border w3-light-grey" name="username" type="text" required>
-
-  <label class="w3-text-teal"><b>Wachtwoord</b></label>
-  <input class="w3-input w3-border w3-light-grey" name="password" type="Password" required>
-  <br/>
-  <input type="submit" class="w3-btn w3-blue-grey" value="Login" name="loginSubmit">
-</form>
-
-<a href="index.php?pageStr=register" id="register">
-	Registreer
-</a>
-
 <?php
+if($user->loginCheck())
+{
+	echo 'U bent al ingelogd';
+}
+else
+{
+	?>
+	<form class="w3-container" method="POST">
+	  <label class="w3-text-teal"><b>Gebruikersnaam of student nummer</b></label>
+	  <input class="w3-input w3-border w3-light-grey" name="username" type="text" required>
+
+	  <label class="w3-text-teal"><b>Wachtwoord</b></label>
+	  <input class="w3-input w3-border w3-light-grey" name="password" type="Password" required>
+	  <br/>
+	  <input type="submit" class="w3-btn w3-blue-grey" value="Login" name="loginSubmit">
+	</form>
+
+	<a href="index.php?pageStr=register" id="register">
+		Registreer
+	</a>
+	<?php
+}
 if($misc->readVar('POST','username') && $misc->readVar('POST','password'))
 {
 	$username = $_POST['username'];
