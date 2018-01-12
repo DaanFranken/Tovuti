ef2a2a26b1dd4a953043d6e0a618611735e2eb37
DaanFranken <dgmfranken@gmail.com>
2018-01-12 12:42:57 +0100

    kedq;kefnq;c w

--- classlist.php
+++ classlist.php
@@ -15,29 +15,46 @@ if($user->loginCheck())
 		{
 			echo '<a class="thread" href="?pageStr=class">Terug naar klassen overzicht</a>';
 		}
-		?>
-		<div class="w3-card-4 w3-rest">
-			<header class="w3-container w3-light-grey">
-				<h3><?php echo 'Klassenlijst '. (isset($_GET['class_id']) ? $result['Name'] : '');   ?></h3>
-			</header>
-			<div class="w3-container">
-				<p>
-					<?php
-					if(!$misc->readVar('GET','class_id'))
-					{
-						$misc->getAllClassesAsList();	
-					}
-					else
-					{
-						$misc->getAllStudentsInClass($_GET['class_id']);
-					}			
+		if(!isset($_POST['addUserToClass']))
+		{
+			?>
+			<div class="w3-card-4 w3-rest">
+				<header class="w3-container w3-light-grey">
+					<h3><?php echo 'Klassenlijst '. (isset($_GET['class_id']) ? $result['Name'] : '');   ?></h3>
+				</header>
+				<div class="w3-container">
+					<p>
+						<?php
+						if(!$misc->readVar('GET','class_id'))
+						{
+							$misc->getAllClassesAsList();	
+						}
+						else
+						{
+							$misc->getAllStudentsInClass($_GET['class_id']);
+						}			
 
-					?></p>
-					<hr>
+						?></p>
+						<hr>
+					</div>
 				</div>
+				<?php if($misc->readVar('GET','class_id'))
+				{
+					?>
+					<form action="" method="POST">
+						<input type="submit" name="addUserToClass" value="+" class="w3-button w3-circle w3-teal w3-right w3-medium w3-margin w3-card-4">
+					</form>	
+					<?php
+				}
+				?>
+
 			</div>
-		</div>
-<?php 
-}
-else echo 'U dient in te loggen om deze pagina te bekijken';
-?>
+			<?php 
+		}
+		else
+		{
+			// Gebruiker aan klas toevoegen
+		}
+	}
+	else echo 'U dient in te loggen om deze pagina te bekijken';
+	?>
