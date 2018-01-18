e1787e4cf0c80c7b3a747e24e01f60c6cf14827f
stijnklomp <32292824+stijnklomp@users.noreply.github.com>
2018-01-18 16:51:27 +0100

    commit

--- uploadForm.php
+++ uploadForm.php
@@ -1,5 +1,6 @@
 <form action="" method="POST" enctype="multipart/form-data">
+	<input type="hidden" name="uploadFile" value="true">
 	<input type="text" name="title" placeholder="Titel...">
-	<input type="file" name="uploadFile" id="uploadFile">Upload een bestand
+	<input type="file" name="uploadFile" id="uploadFile">
 	<input type="submit" name="submitUpload" value="Upload">
 </form>
