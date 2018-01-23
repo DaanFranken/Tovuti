40bb4f39678dad4a9f31bd0f3011966b8c726271
stijnklomp <32292824+stijnklomp@users.noreply.github.com>
2018-01-23 09:08:16 +0100

    KLassenlijst fix

--- addStudentToClass.php
+++ addStudentToClass.php
@@ -7,19 +7,15 @@ $user = new User();
 $user->loginCheck();
 if($user->permission > 1)
 {
-	$sth = $this->db->selectDatabase('teachers', 'user_ID', $user->id, 'AND class_ID = "'.$_GET['class_id'].'"');
-	if($sth->fetch())
+	$sth = $db->selectDatabase('teachers', 'user_ID', $user->id, '');
+	if($row = $sth->fetch())
 	{
-		$sth = $db->selectDatabase('teachers', 'user_ID', $user->id, '');
-		if($row = $sth->fetch())
+		$sth = $db->selectDatabase('students', 'class_ID', '0', '');
+		while($row = $sth->fetch())
 		{
-			$sth = $db->selectDatabase('students', 'class_ID', '0', '');
-			while($row = $sth->fetch())
-			{
-				$sth2 = $db->selectDatabase('users', 'user_ID', $row['user_ID'], '');
-				$row2 = $sth2->fetch();
-				echo '<option value="'.$row['user_ID'].'">'.$row2['Firstname'].' '.$row2['Lastname'].'</option>';
-			}
+			$sth2 = $db->selectDatabase('users', 'user_ID', $row['user_ID'], '');
+			$row2 = $sth2->fetch();
+			echo '<option value="'.$row['user_ID'].'">'.$row2['Firstname'].' '.$row2['Lastname'].'</option>';
 		}
 	}
 }
--- classes/class.Misc.php
+++ classes/class.Misc.php
@@ -110,28 +110,27 @@
 
         public function getAllStudentsInClass($ID)
         {
-            $sth = $this->db->selectDatabase('students', 'class_ID',$ID,'');
-            $result = $sth->fetchAll();
-
-            foreach($result as $res)
+            $check = false;
+            $user = new User();
+            if($user->loginCheck())
             {
-                $user = new User();
-                if($user->loginCheck())
+                if($user->permission > 1)
                 {
-                    if($user->permission > 1)
+                    $sth = $this->db->selectDatabase('teachers', 'user_ID', $user->id, 'AND class_ID = "'.$_GET['class_id'].'"');
+                    if($sth->fetch())
                     {
-                        $sth = $this->db->selectDatabase('teachers', 'user_ID', $user->id, 'AND class_ID = "'.$_GET['class_id'].'"');
-                        if($sth->fetch())
+                        $sth = $this->db->selectDatabase('students', 'class_ID', $_GET['class_id'], '');
+                        while($row = $sth->fetch())
                         {
-                            echo '<form action="class?class_id='.$_GET['class_id'].'" method="POST" style="position: relative;display: inline;left: -6px;"><input type="hidden" name="user_ID" value="'.$user->id.'"><input type="submit" name="remStudentFromClass" value="X" class="w3-btn" style="color: #F1EEEF;background-color: #C2000D;position: relative;height: 41px;opacity: 0.5;border: none;border-bottom: 2px solid #A30005;border-radius: 5px;" title="Verwijder student van klas"></form>';
+                            $sth2 = $this->db->selectDatabase('users', 'user_ID', $row['user_ID'], '');
+                            $row2 = $sth2->fetch();
+                            echo '<form action="class?class_id='.$_GET['class_id'].'" method="POST" style="position: relative;display: inline;left: -6px;"><input type="hidden" name="user_ID" value="'.$row['user_ID'].'"><input type="submit" name="remStudentFromClass" value="X" class="w3-btn" style="color: #F1EEEF;background-color: #C2000D;position: relative;height: 41px;opacity: 0.5;border: none;border-bottom: 2px solid #A30005;border-radius: 5px;" title="Verwijder student van klas"></form><a class="thread" href="account?user_id='.$row['user_ID'].'">'.$row2['Firstname'] .'&nbsp;'.$row2['Lastname'].'</a><br/>';
+                            $check = true;
                         }
                     }
                 }
-                $user->getUserByID($res['user_ID']);
-
-                echo '<a class="thread" href="account?user_id='.$user->id.'">'.$user->firstname .'&nbsp;'.$user->lastname.'</a><br/>';
             }
-            if(empty($result))
+            if(!$check)
             {
                 echo '<div style="color: #6B6B6B;">Er zitten geen studenten in deze klas</div>';
             }
